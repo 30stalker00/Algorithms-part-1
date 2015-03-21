573121c24c1b463844f25df7f77bfdc7d905342b
30stalker00 <30stalker00@gmail.com>
2015-03-21 13:11:39 +0300

    final quick-union with pathfinding

--- Alg.cs
+++ Alg.cs
@@ -7,25 +7,36 @@ namespace algoritm1
             Uf.union(1,2);
             Uf.union(3,4);
             Uf.union(2,3);
-            bool test = Uf.connection(1,5);
+            bool test = Uf.connection(1,4);
             Console.Write("{0}\n", test);
             return 0;
         }
     
     public class uf{
         private int[] id;
+        private int[] sz;
         private int root(int i){
-        while(i != id[i]) i = id[i];
+        while(i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
         return i;
         }
         public uf(int n){
             id = new int[n];
-            for(int i = 0; i < n; i++)
-            id[i] = i;
+            sz = new int[n];
+            for(int i = 0; i < n; i++){
+                id[i] = i;
+                sz[i] = 1;
+            }
 
         }
         public void union(int a, int b){
-            id[root(a)] = root(b);
+            int i = root(a);
+            int j = root(b);
+            if(i == j) return;
+            if(sz[i] < sz[j]) { id[i] = j; sz[i] += 1;}
+            else              { id[j] = 1; sz[j] += 1;}
             }
             
         
